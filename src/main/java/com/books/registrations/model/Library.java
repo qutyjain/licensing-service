ad724192acdc0b8f9cdb05c561d53f253a302b63
Quty <quty.jain@gmail.com>
2018-04-22 13:40:32 -0400

    Cleaning project

--- src/main/java/com/books/registrations/model/Library.java
+++ src/main/java/com/books/registrations/model/Library.java
@@ -1,9 +1,5 @@
 package com.books.registrations.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 public class Library {
 

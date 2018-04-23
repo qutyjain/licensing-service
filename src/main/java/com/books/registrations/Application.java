db4c101eb8d729b7648d958bc60f5835b6017dca
Quty <quty.jain@gmail.com>
2018-04-23 14:01:53 -0400

    Implementing timeout circuit break

--- src/main/java/com/books/registrations/Application.java
+++ src/main/java/com/books/registrations/Application.java
@@ -2,6 +2,7 @@ package com.books.registrations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @RefreshScope
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- src/main/java/com/books/registrations/services/RegisterService.java
+++ src/main/java/com/books/registrations/services/RegisterService.java
@@ -1,6 +1,7 @@
 package com.books.registrations.services;
 
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import com.books.registrations.clients.LibraryDiscoveryClient;
 import com.books.registrations.model.Library;
 import com.books.registrations.model.Register;
 import com.books.registrations.repository.RegisterRepository;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
 public class RegisterService {
@@ -23,7 +25,25 @@ public class RegisterService {
 
 	}
 
-	public Register getRegisterWithLibInfo(String libraryId, String registerId,String clientType) {
+	private void randomlyRunLong() {
+		Random rand = new Random();
+		int randomNum = rand.nextInt((3 - 1) + 1) + 1;
+		if (randomNum == 3)
+			sleep();
+	}
+
+	private void sleep() {
+		try {
+			Thread.sleep(11000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@HystrixCommand
+	public Register getRegisterWithLibInfo(String libraryId, String registerId,
+			String clientType) {
+		randomlyRunLong();
 		Register register = registerRepository.findByregisterId(registerId);
 		Library library = getLibraryInfo(libraryId);
 		register.withLibraryContact(library.getLibraryContact())

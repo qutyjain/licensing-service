4ec74597ebd24d87b53a7327b64a2756ab160966
Quty <quty.jain@gmail.com>
2018-05-09 16:39:33 -0400

    Correcting test class

--- src/test/java/com/books/registrations/ApplicationTests.java
+++ src/test/java/com/books/registrations/ApplicationTests.java
@@ -1,13 +1,14 @@
-package com.example.demo;
+package com.books.registrations;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class DemoApplicationTests {
+@SpringBootTest(classes = Application.class)
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
